97d6c927da6c93403e02c645b8ee51de4c54ae4b
Aquiles Lerman <57549900+aquillesf@users.noreply.github.com>
2025-09-10 15:03:35 -0300

    Update sistema_funcionarios.c

--- sistema_de_funcionarios/sistema_funcionarios.c
+++ sistema_de_funcionarios/sistema_funcionarios.c
@@ -1,3 +1,4 @@
+// Online C compiler to run C program online
 #include <stdio.h>
 
 void limpar_buffer();
@@ -13,19 +14,20 @@ int main() {
     Funcionario funcionarios[100];
     int quantidade = 0;
     char ch, input;
-    printf("olá! seja bem vindo ao registrador de usuários totalmente escrito em C!\n");
+    printf("Olá! seja bem vindo ao registrador de usuários totalmente escrito em C!\n\n");
+    printf("Aperte Enter para começar...\n");
     while((ch = getchar()) != EOF){
         printf("você deseja?\n");
         printf("1 - Adicionar Funcionário\n");
         printf("2 - Remover Funcionário\n");
-        printf("2 - Listar Funcionários\n");
+        printf("3 - Listar Funcionários\n");
         input = getchar();
         
         if(input == '1'){
-            limpar_buffer();
             printf("\n\n");
             printf("Insira o nome do funcionário: ");
             fgets(funcionarios[quantidade].nome, sizeof(funcionarios[quantidade].nome), stdin);
+            limpar_buffer();
             printf("\n");
             printf("Insira o sobrenome do funcionário: ");
             fgets(funcionarios[quantidade].sobrenome, sizeof(funcionarios[quantidade].sobrenome), stdin);
@@ -34,17 +36,41 @@ int main() {
             fgets(funcionarios[quantidade].cargo, sizeof(funcionarios[quantidade].cargo), stdin);
             printf("\n");
             funcionarios[quantidade].ID = quantidade;
-            quantidade ++; 
+            quantidade ++;    
         }
+
         if(input == '2'){
-            limpar_buffer(); 
+            int ID_deletar;
+            printf("Insira o ID do funcionário que deseja deletar: ");
+            scanf("%d", &ID_deletar);
+            limpar_buffer();
+        
+            int encontrado = 0;
+            for(int i = 0; i < quantidade; i++){
+                if(funcionarios[i].ID == ID_deletar){
+                    encontrado = 1;
+                    for(int j = i; j < quantidade - 1; j++){
+                        funcionarios[j] = funcionarios[j + 1];
+                        funcionarios[j].ID = j;
+                    }
+                    quantidade--;
+                    printf("Funcionário com ID %d deletado.\n", ID_deletar);
+                    break;
+                }
+            }
+        
+            if(!encontrado){
+                printf("Funcionário com ID %d não encontrado.\n", ID_deletar);
+            }
         }
+
         if(input == '3'){
-            limpar_buffer();        
+            limpar_buffer();     
         }
     }
     return 0;
 }
+
 void limpar_buffer() {
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
